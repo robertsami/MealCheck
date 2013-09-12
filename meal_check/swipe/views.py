e5a3ffcbce82a1d1995069b3a5eb1584bbe0f53b
Rob <robert.sami@gmail.com>
2013-09-11 20:09:57 -0400

    added swipe

---
+++ meal_check/swipe/views.py
@@ -0,0 +1,12 @@
+# Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from womblings.models import Member
+
+def index(request):
+    member_list = Member.objects.order_by('puid')[:5]
+    template = loader.get_template('swipe/index.html')
+    context = RequestContext(request, {
+        'members' : member_list,
+    })
+    return HttpResponse(template.render(context))
