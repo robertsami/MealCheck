9d9d5b00eb0e2dffb960b4ad74a7787ea23ce4e7
ajwerner <awerner32@gmail.com>
2013-09-14 15:28:15 -0400

    Decoration begins

--- meal_check/meal_check/static/swipe/index.js
+++ meal_check/meal_check/static/swipe/index.js
@@ -3,6 +3,17 @@ var re=/%\d{6}(\d{9})\d=(.*)\/(.*)\?;\d{6}\d{9}\d{4}=\?$/g;
 var timer = $.timer(function() { currInput = "";});
 timer.set({ time: 500, autostart: false});
 
+$.fn.center = function () {
+   this.css("position","absolute");
+   this.css("top", ( $(window).height() - this.height() ) / 2  + "px");
+   this.css("left", ( $(window).width() - this.width() ) / 2 + "px");
+   return this;
+};
+
+$(document).ready( function () {
+    $("#member_status").center();
+});
+
 $(document).keypress(function(event) {
     if (event.keyCode != 13) {
         currInput = currInput.concat(String.fromCharCode(event.keyCode));
--- meal_check/womblings/models/records.py
+++ meal_check/womblings/models/records.py
@@ -15,15 +15,12 @@ class MealExchangeRecord(models.Model):
         app_label = 'womblings'
 
 class SwipeRecord(models.Model):
-	member = models.ForeignKey(Member)
-	swipe_time = models.DateTimeField('date swiped')
+    member = models.ForeignKey(Member)
+    swipe_time = models.DateTimeField('date swiped')
     meal_exchange = models.ForeignKey(MealExchangeRecord)
-
-	def __unicode__(self):
-		return '%s : %s%s' % (self.swipe_time, self.member, ' Had guest' if self.was_allowed else '')
-
-	class Meta:
-	    app_label = 'womblings'
-
-
     
+    def __unicode__(self):
+        return '%s : %s%s' % (self.swipe_time, self.member, ' Had guest' if self.was_allowed else '')
+
+    class Meta:
+        app_label = 'womblings'
